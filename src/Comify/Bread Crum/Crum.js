fa72204c83d7cfc25c5846523996d72b3777781a
Susy2801 <nduy200@gmail.com>
2024-05-06 13:43:37 +0700

    bubble fixed

--- src/Comify/Bread Crum/Crum.js
+++ src/Comify/Bread Crum/Crum.js
@@ -7,6 +7,8 @@ function Crum() {
       <div className="crum__container">
         <img alt="crum" src={anime} className="crum__img" />
         <div className="crum__text right">Chọt em đi anh</div>
+        <div className="bubble"></div>
+        <div className="bubble b2"></div>
       </div>
     </a>
   );
